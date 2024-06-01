4c979b150e5e43405abca6aec15db65a4444d389
Marta Taddese <135144339+martataddese@users.noreply.github.com>
2024-06-01 15:09:40 +0300

    Initial commit

---
+++ src/components/adminEditUser.js
@@ -0,0 +1,131 @@
+import React from "react";
+import axios from 'axios';
+import '../App.css';
+import Navbar from "./navbar";
+
+class AdminEditUser extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userid: this.props.location.userid,
+            username: this.props.location.username,
+            password: this.props.location.password,
+            role: this.props.location.role,
+            archived: this.props.location.archived,
+            firstName: this.props.location.firstName,
+            lastName: this.props.location.lastName,
+            props: this.props
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let props = this.props;
+        let data;
+        if (e.target.password.value != "") {
+            data = {
+                username: this.state.username,
+                password: e.target.password.value,
+                role: this.state.role,
+                archived: this.state.archived,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+            };
+        } else {
+            data = {
+                username: this.state.username,
+                role: this.state.role,
+                archived: this.state.archived,
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+            };
+        }
+
+        axios.put('http://localhost:3000/api/v1/users/' + this.state.userid, data)
+            .then(function (response) {
+                console.log(response);
+                console.log(data);
+                if(response.status === 200){
+                    props.history.push('/admin');
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    render() {
+        const navMenu = ['Admin View','Log-out'];
+        const navMenuLink = ['admin','/'];
+
+        return (
+            <section>
+                <Navbar navMenu={navMenu} navMenuLink={navMenuLink}/>
+                <div className="container">
+                    <div className="row justify-content-sm-center">
+                        <div className="col-sm-6 editform">
+                            <form className="form-signin" onSubmit={(e) => this.handleSubmit(e)}>
+                                <h3>Edit User</h3>
+                                <label htmlFor="inputEmail" className="sr-only">Username</label>
+                                <input type="text" name="username" value={this.state.username}
+                                       className="form-control"
+                                       placeholder="Username"
+                                       onChange={this.handleInputChange}
+                                       required autoFocus/>
+                                <label htmlFor="inputPassword" className="sr-only">Password</label>
+                                <input type="password" name="password" id="inputPassword" className="form-control"
+                                       placeholder="Password" onChange={this.handleInputChange}
+                                       />
+                                <label>First Name</label>
+                                <input type="text" name="firstName" value={this.state.firstName}
+                                       className="form-control"
+                                       placeholder="First Name"  onChange={this.handleInputChange}
+                                       />
+                                <label>Last Name</label>
+                                <input type="text" name="lastName" value={this.state.lastName}
+                                       className="form-control"
+                                       placeholder="Last Name"  onChange={this.handleInputChange}
+                                       />
+
+                                <label>Role</label>
+                                <select name="role" onChange={this.handleInputChange} value={this.state.role}>
+                                    <option value="admin" selected>Admin</option>
+                                    <option value="teacher">Teacher</option>
+                                    <option value="student">Student</option>
+                                </select>
+
+                                <br/>
+
+                                <label>Archived</label>
+
+                                <select name="archived" value={this.state.archived}  onChange={this.handleInputChange}>
+                                    <option value="true" selected>True</option>
+                                    <option value="false">False</option>
+                                </select>
+
+                                <br/>
+
+                                <button className="btn btn-lg btn-primary btn-block" type="submit">Update</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default AdminEditUser;
+
+
